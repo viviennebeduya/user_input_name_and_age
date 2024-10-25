cb29babf9958ddeefed0deaf1109eee65e94cd1c
viviennebeduya <viviennebeduya@gmail.com>
2024-10-25 17:37:54 +0800

    user input loop

--- user_input_name_and_age.py
+++ user_input_name_and_age.py
@@ -1,11 +1,20 @@
 # user input name and age
+user_input = {}
 # print error input=valid
 # --define valid name and valid age
-try:
-    name = str(input("Input a name: "))
-    age = int(input("Input an age: "))
-except:
-    print ("Input invalid.")
+while True:
+    try:
+        name = str(input("Input a name: "))
+        age = int(input("Input an age: "))
+    
+        user_input[name] = {
+            "age": age
+        }
+
+        print (user_input)
+    except:
+        print ("Input invalid.")
+    
 # store infos to array
 # every input, ask if it wants another entry
     # Yes = ask again for input
