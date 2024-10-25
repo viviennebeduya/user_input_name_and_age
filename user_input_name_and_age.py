6e2799e52c582ae8111021d16dbfe2ab68b2fdf9
viviennebeduya <viviennebeduya@gmail.com>
2024-10-25 19:27:38 +0800

    FIX USER'S RETRY

--- user_input_name_and_age.py
+++ user_input_name_and_age.py
@@ -1,5 +1,4 @@
 # array
-
 user_input = {}
 # initialize oldest name,age
 oldest_age = 0
@@ -22,18 +21,21 @@ while True:
                 oldest_age = age
 
             print (f"\nCurrent Directory: \n {user_input}.")
-
-            retry = input ("Would you like to input another (yes/no)? ")
+# ask if user wants another entry
+            while True:
+                retry = input ("Would you like to input another (yes/no)? ")
+                if retry == "no":
+                    break
+                elif retry == "yes":
+                    break
+                else:
+                    print('Input is invalid. Please enter if "yes" or "no"')
             break
 # print error input=valid
         except:
-            print ("Input invalid.")
-# ask if user wants another entry
+            print ("Input Invalid. Please input valid name and age.")
     if retry == "no":
         break
-    elif retry != "yes":
-# print error input=valid
-        print ("Invalid Input.")
 
 print (f"The oldest person is {oldest_name} at the age of {oldest_age}.")
 
